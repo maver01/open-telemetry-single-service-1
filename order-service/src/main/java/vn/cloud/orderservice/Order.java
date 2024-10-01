a757066a3953a8ea0295c88bd5c4a58bec4e1456
maver01 <matteo.sabadin4@gmail.com>
2024-10-01 17:04:21 +0200

    added otel jar

---
+++ order-service/src/main/java/vn/cloud/orderservice/Order.java
@@ -0,0 +1,8 @@
+package vn.cloud.orderservice;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+
+public record Order(Long id, Long customerId, ZonedDateTime orderDate, BigDecimal totalAmount) {
+
+}
